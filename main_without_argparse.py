ca07a08b10f55ce69980a46f85149042842328f1
2haed <101021036+2haed@users.noreply.github.com>
2022-10-07 11:53:41 +0300

    ready project

--- main_without_argparse.py
+++ main_without_argparse.py
@@ -12,7 +12,7 @@ def main():
     X = df.drop(['tea_or_coffee'], axis=1).values
     y = df.tea_or_coffee.values
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
-
+    print(df.corr())
     with KNearestNeighborsClassifier(k=k) as self_made_clf:
         self_made_clf.fit(X=X_train, y=y_train)
         scikit_learn_clf = KNeighborsClassifier(n_neighbors=k)
